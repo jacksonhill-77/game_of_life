bf8b8d174272ae63bfd94fc3525e4e4d9cd28086
jacksonreaction <contactjacksonhill@gmail.com>
2022-10-05 07:58:18 +1100

    Write pseudocode for initial classes

---
+++ game_of_life.py
@@ -0,0 +1,52 @@
+'''Conway's game of life on an infinite grid, optimised for sparse finite life
+
+In Conway's game of life, there's a square grid, where every cell has eight 
+neighbours (sides, up and down, and diagonals). 
+
+Every cell is either alive or dead. We're going to assume almost all cells 
+are dead, and at the beginning of the game the user will provide a list of 
+all the cell coordinates that are alive.
+
+On each step of the game,
+1. Any live cell with two or three live neighbours survives.
+2. Any dead cell with three live neighbours becomes a live cell. (reproduction)
+3. All other live cells die in the next generation (under- or over-population),
+and all other dead cells stay dead.
+
+As well as the list of live cells, the user will specify the number of steps 
+the game should run for. At the end, the game will print the list of live cells.
+
+Extension (if you want it) is to let the user specify a rectangular section of 
+the grid and (using ascii art or otherwise) show the state of the game within 
+that rectangle on each step of the game (kind of like an animation). (Note: 
+any life that ends up outside the rectangle should still be listed at the end 
+of the game.)
+
+You can get some inspiration for possible test cases here: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Examples_of_patterns
+'''
+
+'''
+Class Cell
+
+parameter: alive (boolean)
+x coordinate
+y coordinate
+
+Class Grid
+
+parameter: x width
+parameter: y height
+
+Class GamePlayer
+
+input argument: size of grid 
+input argument: list of coordinates (list of tuples)
+input argument: number of steps the game should run for
+output: list of live cells
+
+function: insert user's coordinates as list into grid
+for each cell:
+    check cells around it
+    if cell's x or y = current cell's x or y +- 1, cell is adjacent
+    if 2 cells are alive
+'''
