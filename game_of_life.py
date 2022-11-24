8da0c5a306af455366e714d29f6781179e4bab4f
jacksonreaction <contactjacksonhill@gmail.com>
2022-11-24 21:38:55 +1100

    Fill in missing logic for simulateStep function

--- game_of_life.py
+++ game_of_life.py
@@ -3,13 +3,13 @@ class Coordinate:
         self.x = x
         self.y = y
 
-def getAdjacentCoordinates(coordinates):
-    adjacentCoordinates = set()
-    for coordinate in coordinates:
-        for x in range(coordinate.x - 1, 2):
-            for y in range (coordinate.y - 1, 2):
-                adjacentCoordinates.add(Coordinate(x, y))
-    return adjacentCoordinates
+    def getAdjacentCoordinates(coordinates):
+        adjacentCoordinates = set()
+        for coordinate in coordinates:
+            for x in range(coordinate.x - 1, 2):
+                for y in range (coordinate.y - 1, 2):
+                    adjacentCoordinates.add(Coordinate(x, y))
+        return adjacentCoordinates
 
 class GamePlayer:
 
@@ -17,31 +17,32 @@ class GamePlayer:
         self.aliveCells = aliveCells
         self.maxSteps = maxSteps
 
-    def countAdjacentLiveCells(self, cell):
-        pass
-        # Incomplete
+    def getAdjacentLiveCoordinates(self, coordinate):
+        print(len(coordinate.getAdjacentCoordinates))
+        return len(coordinate.getAdjacentCoordinates)
 
     def nextGenerationIsAlive(self, coordinate):
-        liveNeighbourCount = self.countAdjacentLivecoordinates(coordinate)
+        liveNeighbourCount = self.getAdjacentLiveCoordinates(coordinate)
         if self.isAlive(coordinate) == True and 2 <= liveNeighbourCount <= 3:
             return True
         elif self.isAlive(coordinate) == False and liveNeighbourCount == 3:
             return True
         else: 
             return False
-
+    
 
     def simulateStep(self):
         nextGenerationOfAliveCells = []
-        coordinatesWeNeedToLookAt = self.getAdjacentCoordinates(self.aliveCells)
-        for coordinate in coordinatesWeNeedToLookAt:
-            if (self.nextGenerationIsAlive(coordinate)):
-                nextGenerationOfAliveCells.append(coordinate)
+        for cell in self.aliveCells:
+            coordinatesWeNeedToLookAt = self.getAdjacentLiveCoordinates(self.aliveCells)
+            for coordinate in coordinatesWeNeedToLookAt:
+                if (self.nextGenerationIsAlive(coordinate)):
+                    nextGenerationOfAliveCells.append(coordinate)
         self.aliveCells = nextGenerationOfAliveCells
             
 
     def playGame(self):
-        for _step in range(self.maxSteps):
+        for step in range(self.maxSteps):
             self.simulateStep()
         return self.aliveCells
 
