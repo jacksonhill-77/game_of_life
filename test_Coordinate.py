d7ce4d95392aa781ab254a8118dc4523c0a094bf
jacksonreaction <contactjacksonhill@gmail.com>
2022-11-27 15:46:45 +1100

    Fix first test for Coordinate class

--- test_Coordinate.py
+++ test_Coordinate.py
@@ -1,7 +1,15 @@
 from Coordinate import Coordinate
 
+def convertCoordinatesToTuples(listOfCoordinates):
+    tupleList = []
+    for coordinate in listOfCoordinates:
+        tupleList.append((coordinate.x,coordinate.y))
+    return tupleList
+
 def testGetAdjacentCellsWorks():
     coordinate = Coordinate(5,5)
     adjacentCells = coordinate.getAdjacentCoordinates()
-    expectedAdjacentCells = [(4,4),(5,4),(6,4),(4,5),(5,5),(6,5),(4,6),(5,6),(6,6)]
-    assert expectedAdjacentCells in adjacentCells == True
+    adjacentCells = convertCoordinatesToTuples(adjacentCells)
+    expectedAdjacentCells = [(4,4),(5,4),(6,4),(4,5),(6,5),(4,6),(5,6),(6,6)]
+    assert len(set(adjacentCells + expectedAdjacentCells)) == 8
+
