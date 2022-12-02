cd3396bc805c6b56e743ee32da2e55b836c9137c
jacksonreaction <contactjacksonhill@gmail.com>
2022-12-03 08:03:37 +1100

    Rename all instances of coordinate to cell for clarity

--- Cell.py
+++ Cell.py
@@ -1,16 +1,16 @@
-class Coordinate:
+class Cell:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-    def getAdjacentCoordinates(self):
-        adjacentCoordinates = []
+    def getAdjacentCells(self):
+        adjacentCells = []
         for y in range(self.y - 1, self.y + 2):
             for x in range (self.x - 1, self.x + 2):
                 if (x,y) == (self.x, self.y):
                     continue
-                adjacentCoordinates.append(Coordinate(x, y))
-        return adjacentCoordinates
+                adjacentCells.append(Cell(x, y))
+        return adjacentCells
 
     def __eq__(self, other):
         if self.x == other.x and self.y == other.y:
--- game_of_life.py
+++ game_of_life.py
@@ -1,42 +1,48 @@
-from Coordinate import Coordinate
+from Cell import Cell
 
 class GamePlayer:
 
-    def __init__(self, aliveCells, maxSteps, gridTopLeftCoordinate, gridX, gridY):
+    def __init__(self, aliveCells, maxSteps, gridOrigin, gridX, gridY):
         self.aliveCells = []
         self.maxSteps = maxSteps
         self.gridX = gridX
         self.gridY = gridY
+        self.gridOriginX = gridOrigin[0]
+        self.gridOriginY = gridOrigin[1]
 
         for cell in aliveCells:
-            coordinate = Coordinate(cell[0],cell[1])
-            self.aliveCells.append(coordinate)
+            newCell = Cell(cell[0],cell[1])
+            self.aliveCells.append(newCell)
 
     def printCurrentGridState(self):
-        cellCount = 0
-        for y in range(self.gridY):
+        for y in range(self.gridOriginY, self.gridY):
             lineOfCells = ""
-            for x in range(self.gridX):
-                coordinate = (x,y)
+            for x in range(self.gridOriginX, self.gridX):
+                cell = Cell(x,y)
+                if cell in self.aliveCells:
+                    lineOfCells += "O "
+                else:
+                    lineOfCells += "- "
             print(lineOfCells)
 
-    def printCoordinatesAsTuples(self, listOfCoordinates):
-        if len(listOfCoordinates) == 0:
+    def printCellsAsTuples(self, listOfCells):
+        if len(listOfCells) == 0:
             print("No cells")
-        for cell in listOfCoordinates:
-            print(cell.x,cell.y)
+        else: 
+            for cell in listOfCells:
+                print(cell.x,cell.y)
 
-    def getAdjacentLiveCoordinates(self, coordinate):
-        adjacentLiveCoordinates = []
-        adjacentCoordinates = coordinate.getAdjacentCoordinates()
-        for coordinate in adjacentCoordinates:
-            for cell in self.aliveCells:
-                if coordinate == cell:
-                    adjacentLiveCoordinates.append(coordinate)
-        return adjacentLiveCoordinates
+    def getAdjacentLiveCells(self, cell):
+        adjacentLiveCells = []
+        adjacentCells = cell.getAdjacentCells()
+        for adjacentCell in adjacentCells:
+            for aliveCell in self.aliveCells:
+                if adjacentCell == aliveCell:
+                    adjacentLiveCells.append(cell)
+        return adjacentLiveCells
 
     def doesCellSurvive(self, cell):
-        liveNeighbourCount = len(self.getAdjacentLiveCoordinates(cell))
+        liveNeighbourCount = len(self.getAdjacentLiveCells(cell))
         if cell not in self.aliveCells and liveNeighbourCount == 3:
             return True
         elif cell in self.aliveCells and 2 <= liveNeighbourCount <= 3:
@@ -56,20 +62,19 @@ class GamePlayer:
         for cell in self.aliveCells:
             if cell not in cellsWeNeedToLookAt:
                 cellsWeNeedToLookAt.append(cell)
-            for adjacentCoordinate in cell.getAdjacentCoordinates():
-                if adjacentCoordinate not in cellsWeNeedToLookAt:
-                    cellsWeNeedToLookAt.append(adjacentCoordinate)
+            for adjacentCell in cell.getAdjacentCells():
+                if adjacentCell not in cellsWeNeedToLookAt:
+                    cellsWeNeedToLookAt.append(adjacentCell)
         return cellsWeNeedToLookAt
 
     def simulateStep(self):
-        print("Live cells starting on this step:")
-        self.printCoordinatesAsTuples(self.aliveCells)
         cellsWeNeedToLookAt = self.findAllCellsToLookAt()
         nextGenerationOfAliveCells = self.determineIfEachCellSurvives(cellsWeNeedToLookAt)
         self.aliveCells = nextGenerationOfAliveCells
 
     def playGame(self):
         for step in range(self.maxSteps):
+            self.printCurrentGridState()
             print("\nStep " + str(step))
             self.simulateStep()
         return self.aliveCells
