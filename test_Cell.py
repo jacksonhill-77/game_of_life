1d5db30f637e7c0a8fb0c529a728c68fc27a3706
jacksonreaction <contactjacksonhill@gmail.com>
2022-12-03 08:08:44 +1100

    Rename all instances of coordinate to cell for clarity in test files as well

--- test_Cell.py
+++ test_Cell.py
@@ -1,4 +1,4 @@
-from Coordinate import Coordinate
+from Cell import Cell
 
 def convertCoordinatesToTuples(listOfCoordinates):
     tupleList = []
@@ -7,15 +7,15 @@ def convertCoordinatesToTuples(listOfCoordinates):
     return tupleList
 
 def testGetAdjacentCellsWorks():
-    coordinate = Coordinate(5,5)
-    adjacentCells = coordinate.getAdjacentCoordinates()
+    coordinate = Cell(5,5)
+    adjacentCells = coordinate.getAdjacentCells()
     adjacentCells = convertCoordinatesToTuples(adjacentCells)
     expectedAdjacentCells = [(4,4),(5,4),(6,4),(4,5),(6,5),(4,6),(5,6),(6,6)]
     assert len(set(adjacentCells + expectedAdjacentCells)) == 8
 
 def testGetAdjacentCellsWorksWithOriginAndNegatives():
-    coordinate = Coordinate(0,0)
-    adjacentCells = coordinate.getAdjacentCoordinates()
+    coordinate = Cell(0,0)
+    adjacentCells = coordinate.getAdjacentCells()
     adjacentCells = convertCoordinatesToTuples(adjacentCells)
     expectedAdjacentCells = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
     assert len(set(adjacentCells + expectedAdjacentCells)) == 8
--- test_GamePlayer.py
+++ test_GamePlayer.py
@@ -1,19 +1,10 @@
 from game_of_life import GamePlayer
-from Coordinate import Coordinate
+from Cell import Cell
 
 def testInitialisationWorks():
     initialLiveCells = [(1,1),(0,1),(2,1)]
     game = GamePlayer(initialLiveCells, 5, [0,0], 5,5)
-    assert game.aliveCells[1] == Coordinate(0,1)
-
-# def testOutcomeExpected():
-#     initialLiveCells = [(1,1),(0,1),(2,1)]
-#     game = GamePlayer(initialLiveCells, 5, [0,0], 5,5)
-#     aliveCells = game.playGame()
-#     expectedOutcome = [()]
-#     for cell in aliveCells:
-#         assert cell in 
-#     assert
+    assert game.aliveCells[1] == Cell(0,1)
 
 def testAliveCellsLiveOrDieCorrectly():
     initialLiveCells = [(0,1),(1,1),(2,1)]
@@ -28,18 +19,18 @@ def testAliveCellsLiveOrDieCorrectly():
 def testDeadCellsLiveOrDieCorrectly():
     initialLiveCells = [(0,1),(1,1),(2,1)]
     game = GamePlayer(initialLiveCells, 1, [0,0], 5,5)
-    cell1 = Coordinate(0,0)
-    cell2 = Coordinate(1,0)
+    cell1 = Cell(0,0)
+    cell2 = Cell(1,0)
     assert game.doesCellSurvive(cell1) == False
     assert game.doesCellSurvive(cell2) == True
 
-def testGetAdjacentLiveCoordinates():
+def testGetAdjacentLiveCells():
     initialLiveCells = [(0,1),(1,1),(2,1)]
     game = GamePlayer(initialLiveCells, 5, [0,0], 5,5)
-    cell = Coordinate(1,1)
-    adjacentLiveCoordinates = game.getAdjacentLiveCoordinates(cell)
-    cell1 = (adjacentLiveCoordinates[0].x,adjacentLiveCoordinates[0].y)
-    cell2 = (adjacentLiveCoordinates[1].x,adjacentLiveCoordinates[1].y)
+    cell = Cell(1,1)
+    adjacentLiveCells = game.getAdjacentLiveCells(cell)
+    cell1 = (adjacentLiveCells[0].x,adjacentLiveCells[0].y)
+    cell2 = (adjacentLiveCells[1].x,adjacentLiveCells[1].y)
     assert cell1 == (0,1)
     assert cell2 == (2,1)
 
