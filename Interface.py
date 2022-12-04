e4075060537b8f7f5f25070ff265f228b35a6a9a
jacksonreaction <contactjacksonhill@gmail.com>
2022-12-04 17:48:58 +1100

    Add init to Interface class

--- Interface.py
+++ Interface.py
@@ -2,7 +2,24 @@ from GamePlayer import GamePlayer
 
 class Interface:
     def __init__(self):
-        self.
+        self.initialLiveCells = input("Please enter the coordinates of the initial live cells as a list of tuple, or press ENTER for the default [(1,1),(0,1),(2,1)]:")
+        if len(self.initialLiveCells) == 0:
+            self.initialLiveCells = [(1,1),(0,1),(2,1)]
+        self.maxSteps = input("Please enter the amount of steps the game should run for, or press ENTER for the default (5)")
+        if len(self.maxSteps) == 0:
+            self.maxSteps = 5
+        self.gridOrigin = input("Please enter the coordinate of the top left corner of the grid to view the output on, or press ENTER for the default (0,0):")
+        if len(self.gridOrigin) == 0:
+            self.gridOrigin = (0,0)
+        self.gridX = input("Please enter the X dimension of the viewing grid, or press ENTER for the default (5):") 
+        if len(self.gridX) == 0:
+            self.gridX = 5
+        self.gridY = input("Please enter the Y dimension of the viewing grid, or press ENTER for the default (5):") 
+        if len(self.gridY) == 0:
+            self.gridY = 5
 
-    def 
+    def runGame(self):
+        game = GamePlayer(self.initialLiveCells,self.maxSteps,self.gridOrigin,self.gridX,self.gridY)
+        print(game)
+
 
